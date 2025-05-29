8b58512f90981ee5bf7b8e9782d30327ca76dfad
Rohit Rai <rohit0235@gmail.com>
2025-05-29 17:21:06 +0530

    for 8yr rdp

--- src/main/java/com/udd/PropertyTax/Demand/DemandRollOver.java
+++ src/main/java/com/udd/PropertyTax/Demand/DemandRollOver.java
@@ -18,6 +18,9 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -52,7 +55,7 @@ public class DemandRollOver {
 		String fYearText = fyr[1].subSequence(2, 4).toString();
 
 		ZoneId zoneId = ZoneId.of("Etc/UTC");
-
+		
 		LocalDateTime fromTime = LocalDateTime.parse(fyr[0] + "-04-01T12:00:00");
 		long taxPeriodFrom = fromTime.atZone(zoneId).toInstant().toEpochMilli();
 
@@ -209,6 +212,198 @@ public class DemandRollOver {
 		return result;
 
 	}
+	
+	
+	@PostMapping("/rollover-rp")
+	public String rolloverrp(@RequestBody String obj, @RequestParam("ulb") String ulbName,
+			@RequestParam("fy") String fYear, @RequestParam("token") String token, @RequestParam("euuid") String euuid) {
+		String result = "";
+		String[] fyr = fYear.split("-");
+
+		String fYearText = fyr[1].subSequence(2, 4).toString();
+
+		ZoneId zoneId = ZoneId.of("Etc/UTC");
+		
+		
+		String fyss[]= {"2018","2019","2020","2021","2022","2023","2024","2025","2026"};
+		Map<String,Long> startDate = new HashMap<String,Long>(); 
+		Map<String,Long> endDate = new HashMap<String,Long>(); 
+		
+
+		String[] fynew = new String[10];
+				int countfuss = fyss.length;
+				System.out.println("hello");
+				for(int i=0; i<countfuss; i++) {
+					
+					LocalDateTime fromTime = LocalDateTime.parse(fyr[0] + "-04-01T12:00:00");
+					long taxPeristartDateodFrom = fromTime.atZone(zoneId).toInstant().toEpochMilli();
+
+					LocalDateTime toTime = LocalDateTime.parse(fyr[1] + "-03-31T23:59:59");
+					long taxPeriodTo = toTime.atZone(zoneId).toInstant().toEpochMilli();
+					
+					startDate.put(fyss[i],taxPeristartDateodFrom);
+					endDate.put(fyss[i],taxPeriodTo);
+				}
+				System.out.println(fynew[0]);
+
+		LocalDateTime fromTime = LocalDateTime.parse(fyr[0] + "-04-01T12:00:00");
+		long taxPeriodFrom = fromTime.atZone(zoneId).toInstant().toEpochMilli();
+
+		LocalDateTime toTime = LocalDateTime.parse(fyr[1] + "-03-31T23:59:59");
+		long taxPeriodTo = toTime.atZone(zoneId).toInstant().toEpochMilli();
+
+		String[] pidAray = obj.split(",");
+
+		ResponseEntity<String> response = null;
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		for (int i = 0; i < pidAray.length; i++) {
+
+			JSONObject searchRequestInfo = new JSONObject();
+			
+			JSONObject userInfo = new JSONObject();
+
+			JSONObject assessmentInfo = new JSONObject();
+
+			JSONObject additionalDetails = new JSONObject();
+
+			JSONObject RequestInfo2 = new JSONObject();
+
+			JSONObject DemandObj = new JSONObject();
+
+			JSONObject PayerObj = new JSONObject();
+
+			JSONObject DemandDetailsObj = new JSONObject();
+			JSONObject DemandDetailsObjs = new JSONObject();
+
+			JSONObject DemandDetailsObj2 = new JSONObject();
+
+			// JSONObject DemandHead = new JSONObject();
+
+			JSONObject Main = new JSONObject();
+			JSONArray Demand = new JSONArray();
+			JSONArray Demand2 = new JSONArray();
+			JSONArray DemandDetails = new JSONArray();
+
+			String[] objArray = pidAray[i].split(":");
+			String uuid = objArray[0];
+			String ptId = objArray[1];
+			String consumerType = objArray[2];
+			 String amount = objArray[3];
+
+//			for Haldwani
+//			String amount = objArray[3];
+//			int amt = Integer.parseInt(amount);
+//			int amtPrcnt = amt * 15 / 100;
+//			int finalAmt = amt + amtPrcnt;
+//			System.out.println(finalAmt);
+			 
+			 userInfo.put("uuid", euuid);
+			 userInfo.put("type", "EMPLOYEE");
+
+			searchRequestInfo.put("api_id", "Rainmaker");
+			searchRequestInfo.put("ver", ".01");
+			searchRequestInfo.put("ts", "");
+			searchRequestInfo.put("action", "_create");
+			searchRequestInfo.put("did", "1");
+			searchRequestInfo.put("key", "");
+			searchRequestInfo.put("msgId", "20170310130900|en_IN");
+			searchRequestInfo.put("authToken", token);
+			searchRequestInfo.put("userInfo", userInfo);
+			
+			
+			
+
+			assessmentInfo.put("tenantId", ulbName);
+			assessmentInfo.put("propertyId", ptId);
+			assessmentInfo.put("financialYear", fyr[0] + "-" + fYearText);
+			assessmentInfo.put("assessmentDate", "1714852041000");
+			assessmentInfo.put("source", "LEGACY_RECORD");
+			assessmentInfo.put("channel", "CFC_COUNTER");
+			assessmentInfo.put("status", "ACTIVE");
+
+			RequestInfo2.put("tt", "tt");
+
+			additionalDetails.put("RequestInfo", RequestInfo2);
+			additionalDetails.put("reassement", "false");
+
+			DemandObj.put("tenantId", ulbName);
+			DemandObj.put("consumerCode", ptId);
+			DemandObj.put("consumerType", consumerType);
+			DemandObj.put("businessService", "PT");
+			//DemandObj.put("taxPeriodFrom", taxPeriodFrom);
+			DemandObj.put("taxPeriodFrom", startDate.get(2019));
+			DemandObj.put("taxPeriodTo", endDate.get(2020));
+			//DemandObj.put("taxPeriodTo", taxPeriodTo);
+
+			DemandDetailsObj.put("taxHeadMasterCode", "PT_TAX");
+			DemandDetailsObj.put("taxAmount", amount);
+			DemandDetailsObj.put("collectionAmount", 0);
+
+//			For Haldwani
+			
+//			DemandDetailsObjs.put("taxHeadMasterCode", "SWATCHATHA_TAX");
+//			DemandDetailsObjs.put("taxAmount", amount);
+//			DemandDetailsObjs.put("collectionAmount", 0);
+
+			Demand2.add(DemandDetailsObj);
+//			For Haldwani
+//			Demand2.add(DemandDetailsObjs);
+
+			PayerObj.put("uuid", uuid);
+
+			DemandObj.put("payer", PayerObj);
+			DemandObj.put("demandDetails", Demand2);
+
+			DemandDetails.add(DemandDetailsObj2);
+			Demand.add(DemandObj);
+
+			additionalDetails.put("Demands", Demand);
+
+			assessmentInfo.put("additionalDetails", additionalDetails);
+
+			assessmentInfo.put("additionalDetails", additionalDetails);
+
+			Main.put("RequestInfo", searchRequestInfo);
+			Main.put("Assessment", assessmentInfo);
+
+			System.out.println("Demand Initiated for PropertId: "+ptId);
+			String fetchUserDetailsUrl = "https://nagarsewa.uk.gov.in/property-services/assessment/_create?tenantId="
+					+ ulbName;
+			try {
+				HttpEntity<String> searchRequesthead = new HttpEntity<String>(Main.toString(), headers);
+				response = restTemplate.postForEntity(fetchUserDetailsUrl, searchRequesthead, String.class);
+
+				 
+
+				String statusCode = response.getStatusCode().toString();
+				String result2 = ptId + ":" + statusCode + "\n";
+				result = result + result2;
+
+				System.out.println("========================Property Count: ======================" + i);
+
+			} catch (Exception e) {
+
+				System.out.println("PropertyId: " + ptId + " .Showing error: error");
+				// String result2 = ptId + ":" + error + "\n";
+				String error = "error";
+				System.out.println(e);
+				String result2 = ptId + ":" + error + "\n";
+				result = result + result2;
+				continue;
+			}
+
+			WriteToFile(result);
+
+		}
+
+		WriteToFile(result);
+
+		return result;
+
+	}
 
 	public static void WriteToFile(String text) {
 		try {
@@ -220,6 +415,23 @@ public class DemandRollOver {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public static void main(String arr[]) {
+String fyss[]= {"2018","2019","2020","2021","2022","2023","2024","2025","2026"};
+ 
+Map<String,String> mp = new HashMap<String,String>();
+
+String[] fynew = new String[10];
+		int countfuss = fyss.length;
+		System.out.println("hello");
+		for(int i=0; i<countfuss; i++) {
+			System.out.println(i);
+			fynew[i]=fyss[i];
+		mp.put(fyss[i],fyss[i]);
+		}
+		System.out.println(mp);
 	}
 
 }
